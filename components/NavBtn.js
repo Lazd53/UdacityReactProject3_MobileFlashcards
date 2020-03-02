9661b6727f901ac50ba3c7192df60486a08cd96d
Alex Love <lazd53@gmail.com>
2020-03-01 18:55:38 -0800

    feat: Created Reusable Nav Button
    Also added NavBtn to All Decks and provided functionality to navigate to 
    the screen.

---
+++ components/NavBtn.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, TouchableWithoutFeedback, StyleSheet} from 'react-native';
+
+
+export default function NavBtn ({text, navigation, to}) {
+  return (
+    <TouchableWithoutFeedback onPress={() => navigation.navigate(to)}>
+      <View style={styles.container}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    width: "70%",
+    borderColor: 'black',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 20
+  }
+})
--- screens/AllDecks.js
+++ screens/AllDecks.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, Button, View, TouchableWithoutFeedback } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import DeckBtn from '../components/DeckBtn';
+import NavBtn from '../components/NavBtn';
 
 class AllDecks extends React.Component {
 
+  navigateToAddDeck(){
+    const { navigation } = this.props;
+    navigation.navigate('Add a Deck')
+  }
+
   render(){
+    const { navigation } = this.props
     const allDecks = {
       multiplication: {
         title: "Multiplication"
@@ -16,15 +23,26 @@ class AllDecks extends React.Component {
       },
       biology: {
         title: "Biology"
+      },
+      wands: {
+        title: "Wands"
+      },
+      defense: {
+        title: "Defense Against the Dark Arts"
       }
     }
+
     const keys = Object.keys(allDecks);
     return (
       <View style={styles.container}>
-        <Text> All Decks </Text>
-        { keys.map( key => {
-          console.log(allDecks[key])
-          return <DeckBtn key={key} DeckInfo = {allDecks[key]}/>})}
+        <ScrollView style={styles.scrollViewStyling}>
+          <View style={styles.decksContainer}>
+          { keys.map( key => {
+            console.log(allDecks[key])
+            return <DeckBtn key={key} DeckInfo = {allDecks[key]}/>})}
+          </View>
+        </ScrollView>
+        <NavBtn text="Add a Deck" navigation={navigation} to="Add a Deck" />
       </View>
     );
   }
@@ -36,5 +54,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 30,
+    paddingBottom: 20,
+    justifyContent: "center",
+    alignItems: "center"
   },
+  scrollViewStyling: {
+    flex: 1,
+    width: "70%",
+    marginBottom: 20
+  },
+  decksContainer: {
+    flexWrap: "wrap",
+    alignContent: "space-between",
+    justifyContent: "space-between",
+    flexDirection: "row",
+
+  }
 });
+
+// <NavBtn text="Add a Deck" callback={this.navigateToAddDeck} />
