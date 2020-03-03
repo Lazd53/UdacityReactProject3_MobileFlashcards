6b126565b522ff97550f9e10a1c42a8eef0e7dfb
Alex Love <lazd53@gmail.com>
2020-03-03 09:10:20 -0800

    feat: AddDeck screen looks and functionality roughed in.

--- screens/AddDeck.js
+++ screens/AddDeck.js
@@ -1,19 +1,58 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
+// Import Components
+import NavBtn from '../components/NavBtn';
 
-export default function AddDeck() {
-  return (
-    <View style={styles.container}>
-      <Text> Add Deck </Text>
-    </View>
-  );
+
+class AddDeck extends React.Component{
+  state = { newDeckName: ""}
+
+  addDeck = () => {
+    const { navigation } = this.props;
+    this.setState( { newDeckName: ""});
+    // TODO Add deck to Async
+    // TODO add deck to Redux store
+    navigation.navigate("All Decks")
+  }
+
+  controlText = (newText) => {
+    this.setState({newDeckName: newText})
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputField}
+          placeholder = "Enter Deck Name"
+          onChangeText = {(text) => this.controlText(text)}
+          value = {this.state.newDeckName}
+        />
+        <NavBtn text="Add Deck" callback={this.addDeck} />
+      </View>
+    );
+  }
 }
+
+export default AddDeck;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "space-around"
   },
-});
+  inputField: {
+      height: 50,
+      fontSize: 20,
+      paddingLeft: 10,
+      width: "70%",
+      borderColor: 'black',
+      borderWidth: 2,
+      borderRadius: 10
+    }
+  }
+);
