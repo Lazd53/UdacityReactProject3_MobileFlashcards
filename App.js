e90b6da3386b68bb6725cd544568515d182b29a6
Alex Love <lazd53@gmail.com>
2020-03-11 17:13:45 -0700

    feat: Nav links to Quizzes, added tab bar icons

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons, Feather, Entypo } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { isIOS } from './utils/helpers';
 
 // Navigation
 import DecksNav from './navigation/DecksNav'
@@ -25,20 +26,46 @@ export default function App(props) {
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         <NavigationContainer>
           <Tab.Navigator initialRouteName="Decks">
-            <Tab.Screen name="Decks" component={DecksNav} />
-            <Tab.Screen name="Groups" component={GroupsNav} />
-            <Tab.Screen name="Quizzes" component={QuizzesNav} />
+            <Tab.Screen
+              name="Decks"
+              component={DecksNav}
+              options={{
+                tabBarIcon: ()=>(<MaterialCommunityIcons size={25} name="cards"/>)
+              }}
+            />
+            <Tab.Screen
+              name="Groups"
+              component={GroupsNav}
+              options={{
+                tabBarIcon: ()=>(
+                  <View style={styles.groupsIcon}>
+                    <MaterialCommunityIcons size={25} name="cards"/>
+                    <MaterialCommunityIcons size={25} name="cards"/>
+                  </View>
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Quizzes"
+              component={QuizzesNav}
+              options={{
+                tabBarIcon: () => <Entypo name="book" size={25} />
+              }}
+            />
           </Tab.Navigator>
         </NavigationContainer>
       </View>
     );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
+  groupsIcon: {
+    flexDirection: "row"
+  }
 });
+
+            // <Tab.Screen name="Quizzes" component={QuizzesNav} />
