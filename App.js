d6c71d7817af15c69bdb2618387b67eca7add8e4
Alex Love <lazd53@gmail.com>
2020-03-13 14:11:46 -0700

    feat: Added thunk & logger middleware

--- App.js
+++ App.js
@@ -2,17 +2,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 import BottomTabNav from './navigation/BottomTabNav';
 import reducers from './reducers';
+import middleware from './middleware';
 
 const testchange = "test"
-const store = createStore(reducers);
+const store = createStore(reducers, middleware);
 
 export default function App() {
     return (
-      <Provider store= {store }>
+      <Provider store= { store }>
         <View style={styles.container}>
           <BottomTabNav />
         </View>
---
+++ middleware/index.js
@@ -0,0 +1,16 @@
+import {applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+    console.log('The action: ', action)
+    const result = next(action)
+    console.log('The new state: ', store.getState())
+  console.groupEnd()
+  return result
+}
+
+export default applyMiddleware(
+  thunk,
+  logger
+)
