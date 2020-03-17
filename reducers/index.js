460894ce83c45b71b41374277c4dc897e26045c6
Alex Love <lazd53@gmail.com>
2020-03-17 12:00:18 -0700

    feat: Quiz added to combineReducers

--- reducers/index.js
+++ reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {Decks} from './Decks';
 import {Groups} from './Groups';
 import {CurrentSelection} from './CurrentSelection';
+import { Quiz } from './Quiz';
 
 export default combineReducers({
   Decks,
   Groups,
-  CurrentSelection
+  CurrentSelection,
+  Quiz
 })
